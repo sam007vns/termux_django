d0b30d17b55426a712dfd5104a664d4258a801aa
Sameer Verma <sameerverma@sameer.bbrouter>
2021-04-09 10:43:06 +0530

    Addeed photos

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.home,name="home"),
+    path('',views.login,name="login"),
     path('click_photo',views.click_photo,name="click_photo"),
+    path('photos',views.photos,name="photos"),
+    path('home',views.home,name="home"),
+    path('logout',views.logout,name="logout"),
 ]
 
 if settings.DEBUG:
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from .models import *
 from django.contrib.auth.models import auth, User
 from django.contrib import auth
 from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from datetime import datetime, date
 from main import views
@@ -19,9 +20,10 @@ from django.http import FileResponse
 from django.utils.crypto import get_random_string
 from pimux import function
 
+@login_required(login_url='login')
 def home(request):
 	return render(request,"home.html")
-
+@login_required(login_url='login')
 def click_photo(request):
 	x=function.camera()
 	img_name=get_random_string(length=16)+".jpg"
@@ -30,5 +32,26 @@ def click_photo(request):
 	click.save()
 	messages.add_message(request,messages.SUCCESS,"Image Clicked Successfuly!")
 	return redirect(home)
+@login_required(login_url='login')
 def photos(request):
-	return render(request,"photos.html")
+	data=clicked_photo.objects.all()
+	return render(request,"photos.html",{"data":data})
+
+def login(request):
+	if request.method=="POST":
+		username = request.POST.get('username','')
+		password = request.POST.get('password','')
+		user = auth.authenticate(username=username, password=password)
+		if user:
+			auth.login(request,user)
+			return redirect("home")
+		else:
+			messages.add_message(request,messages.WARNING,"Invalid Username or Password")
+			return render(request,"login.html")
+	else:
+		return render(request,"login.html")
+
+@login_required(login_url='login')
+def logout(request):
+	auth.logout(request)
+	return redirect('login')
